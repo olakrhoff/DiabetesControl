774662deb1b77d7ca2af5c9b4665f48ec81a3dec
olakristofferhoff <ola.kr@hotmail.no>
2022-02-03 15:20:24 +0100

    Betaversjon

--- DiabetesContolApp/App.xaml.cs
+++ DiabetesContolApp/App.xaml.cs
@@ -2,15 +2,21 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using DiabetesContolApp.Views;
+
 namespace DiabetesContolApp
 {
     public partial class App : Application
     {
+        //Keys
+        private const string BaseSensitivityKey = "BaseSensitivity";
+        private const string SenesitivityWithoutFoodKey = "SenesitivityWithoutFood";
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage(); //kommentar
+            MainPage = new NavigationPage(new MainTabbedPage()); //kommentar
         }
 
         protected override void OnStart()
@@ -24,5 +30,40 @@ namespace DiabetesContolApp
         protected override void OnResume()
         {
         }
+
+        public int BaseSensitivity
+        {
+            get
+            {
+                if (Properties.ContainsKey(BaseSensitivityKey))
+                {
+                    return (int)Properties[BaseSensitivityKey];
+                }
+                return 0; //Returns -1 if there is no prpoertiy sat
+            }
+
+            set
+            {
+                Properties[BaseSensitivityKey] = value;
+            }
+        }
+
+        public float SenesitivityWithoutFood
+        {
+            get
+            {
+                if (Properties.ContainsKey(SenesitivityWithoutFoodKey))
+                {
+                    return (float)Properties[SenesitivityWithoutFoodKey];
+                }
+                return 0f;
+            }
+
+            set
+            {
+                Properties[SenesitivityWithoutFoodKey] = value;
+            }
+        }
+
     }
 }
