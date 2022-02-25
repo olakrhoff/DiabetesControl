e1d943f58d60f2f283353d2712299204f8b46ded
olakrhoff <ola.kr@hotmail.no>
2022-02-25 09:48:16 +0100

    Statistics for timeusage insaide app added

---
+++ DiabetesContolApp/Views/SettingsAndStatisticsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DiabetesContolApp.Views
+{
+    public partial class SettingsAndStatisticsPage : ContentPage
+    {
+        public SettingsAndStatisticsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = Application.Current as App;
+        }
+
+        protected override void OnAppearing()
+        {
+            BindingContext = Application.Current as App;
+
+            base.OnAppearing();
+        }
+    }
+}
