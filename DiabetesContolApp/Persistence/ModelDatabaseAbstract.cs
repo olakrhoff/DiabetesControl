ffefd6b38de00c44fec9e16051c78f175227fb60
olakristofferhoff <ola.kr@hotmail.no>
2022-03-02 08:59:27 +0100

    ModelDatabaseAbstract added

---
+++ DiabetesContolApp/Persistence/ModelDatabaseAbstract.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DiabetesContolApp.Models;
+using SQLite;
+using Xamarin.Forms;
+
+namespace DiabetesContolApp.Persistence
+{
+    public abstract class ModelDatabaseAbstract
+    {
+        protected readonly SQLiteAsyncConnection connection;
+
+        public ModelDatabaseAbstract()
+        {
+            connection = DependencyService.Get<ISQLiteDB>().GetConnection();
+
+            connection.CreateTableAsync<DayProfileModel>().Wait();
+            connection.CreateTableAsync<GroceryModel>().Wait();
+            connection.CreateTableAsync<ReminderModel>().Wait();
+            connection.CreateTableAsync<LogModel>().Wait();
+            connection.CreateTableAsync<GroceryLogModel>().Wait();
+        }
+    }
+}
