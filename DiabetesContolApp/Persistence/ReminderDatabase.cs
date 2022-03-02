3e8b07f8dcbb385e1df7c3ceac35e62317b5e72b
olakristofferhoff <ola.kr@hotmail.no>
2022-03-02 09:23:03 +0100

    ReminderDatabase added

---
+++ DiabetesContolApp/Persistence/ReminderDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using DiabetesContolApp.Models;
+using SQLite;
+using Xamarin.Forms;
+
+namespace DiabetesContolApp.Persistence
+{
+    public class ReminderDatabase : ModelDatabaseAbstract
+    {
+
+        private static ReminderDatabase instance = null;
+
+        private LogDatabase logDatabase = LogDatabase.GetInstance();
+
+        public ReminderDatabase()
+        {
+        }
+
+        public static ReminderDatabase GetInstance()
+        {
+            return instance == null ? new ReminderDatabase() : instance;
+        }
+
+        async internal Task<int> InsertReminderAsync(ReminderModel reminderModel)
+        {
+            return await connection.InsertAsync(reminderModel);
+        }
+    }
+}
