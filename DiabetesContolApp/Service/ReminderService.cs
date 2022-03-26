20d5df395de42dc6b18666141561f26a3c31446d
olakristofferhoff <ola.kr@hotmail.no>
2022-03-26 20:36:31 +0100

    RemidnerService is now finished

--- DiabetesContolApp/Service/ReminderService.cs
+++ DiabetesContolApp/Service/ReminderService.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Threading.Tasks;
+
 using DiabetesContolApp.Models;
+using DiabetesContolApp.Repository;
 
 namespace DiabetesContolApp.Service
 {
     public class ReminderService
     {
+        private ReminderRepo reminderRepo;
+
         public ReminderService()
         {
         }
 
+        /// <summary>
+        /// Gets the ReminderModel with the given ID.
+        /// </summary>
+        /// <param name="reminderID"></param>
+        /// <returns>ReminderModel with given ID or null if not found.</returns>
         async public Task<ReminderModel> GetReminderAsync(int reminderID)
         {
-            throw new NotImplementedException();
+            return await reminderRepo.GetAsync(reminderID);
         }
     }
 }
