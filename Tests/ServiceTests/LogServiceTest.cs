12f7a576d6deea1ecf1d6b6c61a9327a7bd12af4
olakrhoff <ola.kr@hotmail.no>
2022-04-11 17:58:58 +0200

    Trying tests

---
+++ Tests/ServiceTests/LogServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Moq;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using DiabetesContolApp.Service;
+using DiabetesContolApp.Repository;
+using DiabetesContolApp.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class LogServiceTest
+    {
+        private LogService logService;
+        private Mock<LogRepo> logRepo;
+
+        [SetUp]
+        public void Setup()
+        {
+            logRepo = new();
+            logService = new();
+        }
+
+        [Test]
+        async public Task InsertLog_ValidLog_ReturnTrue()
+        {
+            try
+            {
+                DayProfileModel dayProfile = new();
+                List<NumberOfGroceryModel> numberOfGroceries = new List<NumberOfGroceryModel> { new(0, new(), 0), new(0, new(), 0) };
+
+                LogModel newLog = new(dayProfile, new(), DateTime.Now, 5.6f, 5.5f, 5.4f, numberOfGroceries);
+
+                bool result = await logService.InsertLogAsync(newLog);
+
+                Assert.AreEqual(result, true);
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                Debug.WriteLine(e.Message);
+                Assert.Fail();
+            }
+
+        }
+    }
+}
